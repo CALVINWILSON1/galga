b0a1df3f712d79a038eb49545d5693e14c5198da
CALVINWILSON1 <92391915+CALVINWILSON1@users.noreply.github.com>
2021-10-21 13:12:15 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,78 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . 4 4 4 4 4 4 4 2 . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpacePlane, 0, 0)
+    projectile.setVelocity(100, 0)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    bogey.setFlag(SpriteFlag.AutoDestroy, true)
+    bogey.destroy(effects.fire, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    SpacePlane.destroy(effects.fire, 500)
+    scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+})
+let projectile: Sprite = null
+let bogey: Sprite = null
+let SpacePlane: Sprite = null
+SpacePlane = sprites.create(img`
+    ..ccc.........ffffff....
+    ..f4cc.......fcc22ff....
+    ..f44cc...fffccccff.....
+    ..f244cccc22224442cc....
+    ..f224cc2222222244b9c...
+    ..cf222222222222b9999c..
+    .c22c222222222911199b2c.
+    f22ccccccc22229911b2222c
+    fffffcc222c222222222222f
+    .....f2224442222222222f.
+    ....f222444fc2222222ff..
+    ...c222444ffffffffff....
+    ...c2222cfffc2f.........
+    ...ffffffff2ccf.........
+    .......ffff2cf..........
+    ........fffff...........
+    `, SpriteKind.Player)
+controller.moveSprite(SpacePlane, 200, 200)
+SpacePlane.setStayInScreen(true)
+info.setLife(3)
+bogey.setVelocity(50, 50)
+game.onUpdateInterval(1000, function () {
+    bogey = sprites.create(img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c b b b b b b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b c b b b c b b b b f . . . . 
+        f b 1 f f f 1 b b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `, SpriteKind.Enemy)
+    bogey.setVelocity(-100, 0)
+    bogey.setPosition(164, randint(5, 115))
+})
